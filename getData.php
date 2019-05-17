400726695a89108a6507eba2783c1133d78bdb33
yinliuxuan <yinliuxuan@gmail.com>
2019-05-17 16:23:12 +0800

    临时保存

--- getData.php
+++ getData.php
@@ -1,16 +1,31 @@
 <?php
 header('Content-Type:text/json;charset=utf-8');
 
-$manager = new MongoDB\Driver\Manager("mongodb://127.0.0.1:27017");
-$filter = [];
-$options = ['limit' => 250];
-$getListQuery = new MongoDB\Driver\Query($filter, $options);
-try {
-    $cursor = $manager->executeQuery('web.films', $getListQuery);
-} catch (Exception $e) {
-    echo 'Query Error';
-}
-echo json_encode(iterator_to_array($cursor));
-//foreach ($cursor as $elem){
-//    print_r($elem);
+//$manager = new MongoDB\Driver\Manager("mongodb://localhost:27017");
+//$filter = [];
+//$options = ['limit' => 250];
+//$getListQuery = new MongoDB\Driver\Query($filter, $options);
+//try {
+//    $cursor = $manager->executeQuery('web.films', $getListQuery);
+//} catch (Exception $e) {
+//    echo 'Query Error';
 //}
+//echo json_encode(iterator_to_array($cursor));
+
+class MongoDBClient{
+    private $mongoDB;
+    private $DBName;
+    private $collection;
+
+    public function _construct(){
+        $this->mongoDB=new \MongoDB\Driver\Manager("mongodb://localhost:27017");
+        $this->DBName='web';
+        $this->collection='films';
+    }
+
+    public function getPageNum(){
+        $command=new \MongoDB\Driver\Command(['count'=>$this->collection]);
+        $result=$this->mongoDB->executeCommand($this->DBName,$command);
+
+    }
+}
